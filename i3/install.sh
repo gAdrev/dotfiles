aba5ced30bade1a032fa7f376e0b156e320febff
Javier V <javiervilla00@gmail.com>
2019-10-15 14:09:10 +0200

    i3: update configuration

--- i3/install.sh
+++ i3/install.sh
@@ -9,5 +9,5 @@ SCRDIR="$(readlink -f $(dirname "$0"))"
 CP_FLAGS="-vi"  # default cp options
 
 cp $CP_FLAGS "$SCRDIR/config" "$HOME/.config/i3/config"
-cp $CP_FLAGS "$SCRDIR/.i3status" "$HOME/.i3status"
+cp $CP_FLAGS "$SCRDIR/.i3status.conf" "$HOME/.i3status.conf"
 
