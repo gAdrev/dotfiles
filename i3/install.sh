dd12652a4201f690562ced65e702a2b2858f89aa
Javier V <javiervilla00@gmail.com>
2019-10-17 16:10:51 +0200

    i3: generate bindings to workspace based on current monitors

--- i3/install.sh
+++ i3/install.sh
@@ -4,10 +4,30 @@
 # required by each application.
 # Add any new files here.
 
-SCRDIR="$(readlink -f $(dirname "$0"))"
+SCRDIR="$(readlink -f "$(dirname "$0")")"
 
 CP_FLAGS="-vi"  # default cp options
 
-cp $CP_FLAGS "$SCRDIR/config" "$HOME/.config/i3/config"
+genconfig() {
+    SECONDARY_NAME=$(xrandr | grep " connected" | grep -v primary | cut -d' ' -f1)
+    TEMP_CONFIG="$(mktemp)"
+    cat <<EOF >"$TEMP_CONFIG"
+workspace 1 output primary
+workspace 2 output "$SECONDARY_NAME"
+workspace 3 output primary
+workspace 4 output "$SECONDARY_NAME"
+workspace 5 output primary
+workspace 6 output "$SECONDARY_NAME"
+workspace 7 output primary
+workspace 8 output "$SECONDARY_NAME"
+workspace 9 output primary
+workspace 10 output "$SECONDARY_NAME"
+EOF
+    cat "$SCRDIR/config" "$TEMP_CONFIG" > "$SCRDIR/config.gen"
+}
+
+genconfig
+cp $CP_FLAGS "$SCRDIR/config.gen" "$HOME/.config/i3/config"
+
 cp $CP_FLAGS "$SCRDIR/.i3status.conf" "$HOME/.i3status.conf"
 
