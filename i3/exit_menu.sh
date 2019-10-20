74fad0f3ac3834e48967b8f4a8cc75060da5384c
Javier V <javiervilla00@gmail.com>
2019-10-20 17:35:42 +0200

    i3: augment exit menu

--- i3/exit_menu.sh
+++ i3/exit_menu.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-while [ "$select" != "NO" -a "$select" != "YES" ]; do
-    select=$(echo -e 'NO\nYES' | dmenu -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i -p "You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.")
+# shellcheck disable=SC2034
+declare -A opts=( [NO]="" [YES]="" [SHUTDOWN]="" [REBOOT]="" )
+
+#while [ "$select" != "NO" -a "$select" != "YES" ]; do
+while ! [ -v opts["$select" ] ]
+do
+    select=$(echo -e 'NO\nYES\nSHUTDOWN\nREBOOT' | \
+    dmenu \
+        -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' \
+        -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i \
+        -p "You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.")
     [ -z "$select" ] && exit 0
 done
 
-[ "$select" = "NO" ] && exit 0
-
-i3-msg exit
+case "$select" in
+        "NO")          exit 0 ;;
+        "YES")         i3-msg exit ;;
+        "SHUTDOWN")    sudo shutdown -hP now ;;
+        "REBOOT")      sudo reboot ;;
+        *)             echo "Bad option" | dmenu ;;  # shouldn't ever happen 
+esac
