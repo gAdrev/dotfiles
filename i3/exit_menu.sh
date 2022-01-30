d044ccf3d2940a4bcb351b48fe30a79bcf360c6f
Javier Villa <javiervilla00@gmail.com>
2022-01-30 21:13:34 +0100

    i3 files

--- i3/exit_menu.sh
+++ i3/exit_menu.sh
@@ -6,7 +6,7 @@ declare -A opts=( [NO]="" [YES]="" [SHUTDOWN]="" [REBOOT]="" )
 #while [ "$select" != "NO" -a "$select" != "YES" ]; do
 while ! [ -v opts["$select"] ]
 do
-    select=$(echo -e 'NO\nYES\nSHUTDOWN\nREBOOT' | \
+    select=$(echo -e 'NO\nYES\nSHUTDOWN\nREBOOT\nHIBERNATE' | \
     dmenu \
         -nb '#151515' -nf '#999999' -sb '#f00060' -sf '#000000' \
         -fn '-*-*-medium-r-normal-*-*-*-*-*-*-100-*-*' -i \
@@ -14,10 +14,19 @@ do
     [ -z "$select" ] && exit 0
 done
 
+function pre_shutdown() {
+    # bug: https://bugs.launchpad.net/ubuntu/+source/pcmanfm/+bug/1878625
+    # They're not pulling the fix upstream for ubuntu 20
+    # pcmanfm gets killed after a 90 second delay on shutdown
+    # (along with gdbus)
+    killall pcmanfm
+}
+
 case "$select" in
         "NO")          exit 0 ;;
         "YES")         i3-msg exit ;;
-        "SHUTDOWN")    sudo shutdown -hP now ;;
-        "REBOOT")      sudo reboot ;;
+        "SHUTDOWN")    pre_shutdown ; sudo shutdown -hP now ;;
+        "REBOOT")      pre_shutdown ; sudo reboot ;;
+        "HIBERNATE")   sudo systemctl hibernate ;;
         *)             echo "Bad option" | dmenu ;;  # shouldn't ever happen 
 esac
